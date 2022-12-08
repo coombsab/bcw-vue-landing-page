a5b97a21e51948e1bd5689a7e51ad9e850520367
Abraham Coombs <coombsab@gmail.com>
2022-12-08 15:20:16 -0700

    initial setup

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="wrapper d-flex align-items-center justify-content-center text-visible">
+    <h1>This is my landing page. Spiffy, right?</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.wrapper {
+  background-color: rgba(0, 0, 0, 0.9);
+  flex-grow: 1;
+}
+</style>
